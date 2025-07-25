be9529d02db8549b7eca784f9d5104ca1faa0127
jaesu968 <mazterk968@gmail.com>
2025-07-25 12:05:51 -0500

    updated router file

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
 // The router file is used to define the routes for the application.
 // and manage the navigation between the pages or views in a single-page application (SPA).
-import ShoppingList from '../views/ShoppingList.vue';
+import ShoppingList from '../views/ShoppingListView.vue';
 import { createRouter, createWebHistory } from 'vue-router'; 
 import CreateItem from '../views/CreateItem.vue'; 
 import UpdateItem from '../views/UpdateItem.vue';
-import Detail from '../views/DetailView.vue';
+import DetailView from '../views/DetailView.vue';
 
 
 const routes = [
@@ -25,9 +25,15 @@ const routes = [
     },
     {
         path: '/detail/:id',
-        name: 'Detail',
+        name: 'DetailView',
         component: DetailView,
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('../views/NotFound.vue') // Lazy loading the NotFound component
+    }
+
 ]; 
 
 const router = createRouter({

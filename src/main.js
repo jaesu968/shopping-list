072b4aa295c47f5de8adce4d70622d473ea7975e
jaesu968 <mazterk968@gmail.com>
2025-07-25 11:35:24 -0500

    Created Views and updated main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router.js'
+
+App.use(router); 
 
 
 createApp(App).mount('#app')
